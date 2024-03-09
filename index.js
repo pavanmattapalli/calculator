5d16819da7ff03d4b9dc7f7a3fde504fb9027501
Pavan Mattapalli <79364585+pavanmattapalli@users.noreply.github.com>
2024-03-09 18:12:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// calcultor logic;
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+ 
+}
+
+function ClearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    display.value = eval(display.value);    
+
+}
